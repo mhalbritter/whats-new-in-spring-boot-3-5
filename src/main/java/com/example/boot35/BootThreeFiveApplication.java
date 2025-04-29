107517564595bde5e462c7eb7497557773345679
Moritz Halbritter <moritz.halbritter@broadcom.com>
2025-04-29 14:33:15 +0200

    Import code

---
+++ src/main/java/com/example/boot35/BootThreeFiveApplication.java
@@ -0,0 +1,13 @@
+package com.example.boot35;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootThreeFiveApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootThreeFiveApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/boot35/BootThreeFiveApplicationTests.java
@@ -0,0 +1,14 @@
+package com.example.boot35;
+
+import org.junit.jupiter.api.Test;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootThreeFiveApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
